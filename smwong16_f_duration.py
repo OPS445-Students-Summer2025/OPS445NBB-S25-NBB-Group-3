5880d985a3a51ccf03e54e05c9643e40a812570d
Sing Man Wong <smwong16@myseneca.ca>
2025-07-28 12:56:04 -0400

    Added v2 with validate duration days

--- smwong16_f_duration.py
+++ smwong16_f_duration.py
@@ -2,3 +2,16 @@
 def calculate_salary_from_duration(staff_info, duration_days, no_pay_days, overtime_hours):
     days_worked = duration_days - no_pay_days
     return calculate_salary(staff_info, days_worked, overtime_hours)
+
+# Function 8: If selected "duration" ask user to input no-pay days and overtime hours and calculate before tax 
+def calculate_duration(staff_info):
+    # Validate duration_days
+    while True:
+        try:
+            duration_days = int(input('Enter duration in days: '))
+            if duration_days <= 0:
+                print('Duration must be a positive number. Please input again.')
+            else:
+                break
+        except ValueError:
+            print('Invalid input for duration. Please enter an integer.')
