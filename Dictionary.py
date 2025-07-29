20f72db829f5629ff8e3efed4f69870d0e33b357
Thit Lwin Oo <132836663+who-is-M4TT@users.noreply.github.com>
2025-07-29 02:30:35 -0400

    Adding CSV

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+import csv
+def load_staff_data(filename):
+    staff_data = {}
+    f = open(filename, 'r', newline='')
+    reader = csv.DictReader(f, skipinitialspace=True)
+    for row in reader:
+        staff_id = row['Staff ID: ']
+        staff_data[staff_id] = {
+            'name': row['Staff Name:'],
+            'type': row['Monthly / Hourly:'],
+            'salary': float(row['Salary:'])
+        }
+    f.close()
+    return staff_data
